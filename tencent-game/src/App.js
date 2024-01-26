358d57d4acabeda73daddd79f3749787f5d7902c
Ebenezer <ebenirene5@gmail.com>
2024-01-27 00:22:15 +0100

    aadded the necessary components and styling

---
+++ tencent-game/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Die from "./component/Die"
+
+
+export default function App() {
+  return (
+    <main>
+        <div className="dice-container">
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+          <Die value="1" />
+
+
+        </div>
+    </main>
+    
+  )
+}
